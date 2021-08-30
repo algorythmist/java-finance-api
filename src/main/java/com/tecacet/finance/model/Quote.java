dabf750e6c8103d38800ff15a49f82664e914607
algorythmist <dimitri@tecacet.com>
2021-08-30 07:28:00 -0700

    cleanup

--- src/main/java/com/tecacet/finance/model/Quote.java
+++ src/main/java/com/tecacet/finance/model/Quote.java
@@ -1,8 +1,11 @@
 package com.tecacet.finance.model;
 
+import lombok.Getter;
+
 import java.io.Serializable;
 import java.time.LocalDate;
 
+@Getter
 public class Quote implements Serializable {
 
     private LocalDate date;
@@ -12,34 +15,6 @@ public class Quote implements Serializable {
     private double close;
     private long volume;
     private double adjustedClose;
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public double getOpen() {
-        return open;
-    }
-
-    public double getHigh() {
-        return high;
-    }
-
-    public double getLow() {
-        return low;
-    }
-
-    public double getClose() {
-        return close;
-    }
-
-    public long getVolume() {
-        return volume;
-    }
-
-    public double getAdjustedClose() {
-        return adjustedClose;
-    }
 
     @Override
     public String toString() {
